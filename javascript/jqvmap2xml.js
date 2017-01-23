3c2429dad09121e85845525767ad53586c52b019
Teddy Payet <teddy.payet@gmail.com>
2017-01-23 14:46:38 +0100

    Du responsive
    permettre que chaque carte soit responsive.

--- javascript/jqvmap2xml.js
+++ javascript/jqvmap2xml.js
@@ -9,4 +9,18 @@ jQuery(document).ready(function (){
 		});
 	});
 
+	resize_map_height();
+	jQuery(window).on('resize', function (){
+		resize_map_height();
+	});
+
+	function resize_map_height() {
+		jQuery('.map').each(function (){
+			var ref_width = jQuery(this).width();
+			jQuery(this).css({
+				'height': ref_width+'px'
+			});
+		});
+	}
+
 });
